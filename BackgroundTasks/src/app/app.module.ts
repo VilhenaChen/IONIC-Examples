6930efaf0f1b6be14b5ceb01a4cd91bdca47194b
Ines Teixeira <issteixeira@gmail.com>
2020-12-30 19:26:34 +0000

    terceiro exemplo le do ficheiro e começa a modificar numeros

--- BackgroundTasks/src/app/app.module.ts
+++ BackgroundTasks/src/app/app.module.ts
@@ -9,12 +9,15 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import {File} from '@ionic-native/file/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
+    File,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
--- BackgroundTasks/src/app/home/back-service.service.ts
+++ BackgroundTasks/src/app/home/back-service.service.ts
@@ -1,32 +1,60 @@
+import { File } from '@ionic-native/file/ngx';
+
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackService {
 
-  private lowest : BehaviorSubject<number>;
-  private highest : BehaviorSubject<number>;
+  
+  
   private lowestNbr : number;
   private highestNbr : number;
+  private lowest : BehaviorSubject<number>;
+  private highest : BehaviorSubject<number>;
   private stop : boolean;
-  constructor() { 
+  private primeiro : boolean;
+  constructor(/*private file : File*/) { 
+    console.log('inicio');                                                                                                                      
     this.lowestNbr = 10000;
     this.highestNbr = 99999;
     this.lowest = new BehaviorSubject<number>(this.lowestNbr);
     this.highest = new BehaviorSubject<number>(this.highestNbr);
     this.stop = true;
+    this.primeiro = true;
   }
 
   async start()
   {
     while(this.stop == false){
+      const loadNumeros = async() =>{
+        const response = await fetch('./assets/numeros.json');
+        const json = await response.json();
+        this.lowestNbr = json['lowest'];
+        this.highestNbr = json['highest'];
+        this.lowest.next(this.lowestNbr);
+        this.highest.next(this.highestNbr);
+        console.log('a');
+      }
+      console.log( );
+      
+      if(this.primeiro == true){
+      await loadNumeros();
+      this.primeiro = false;}
+      await this.delay(10000);
+      
       this.lowestNbr++;
       this.highestNbr--;
       this.lowest.next(this.lowestNbr);
       this.highest.next(this.highestNbr);
-      await this.delay(10000);
+      //this.file.writeFile(this.file.externalApplicationStorageDirectory,'numeros2.json',JSON.stringify({'lowest' : this.lowestNbr, 'highest': this.highestNbr} ),{replace:true});
+      //this.file.writeFile('./','numeros2.json',JSON.stringify({'lowest' : this.lowestNbr, 'highest': this.highestNbr} ),{replace:true});
+      console.log('ta');
+      
     }
   }
 
